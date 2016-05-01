2fb14ad75590c446c5d29d186519032a92e7d2dc
nathanuphoff <Nathan@MacBook-Pro-Nathan.local>
2016-05-01 15:05:16 +0200

    Creating new Master
    Glitch.js and its friends.

---
+++ glitch.js
@@ -0,0 +1,162 @@
+var glitch = function(){ "use strict";
+
+	var support = /[jpeg|png|gif]+/i;
+	var	charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+	
+	// Public methods
+	// A) Initiate a new glitch() image
+	function glitch(source, callback, strength, attempts){
+		
+		if (typeof source !== "string") return error("An image source is required");
+		if (typeof callback !== "function") return error("A callback Function is required");
+		
+		strength = limit(strength, 5, 100) || 25;
+		attempts = limit(attempts, 5, 100) || 50;
+									
+		var isBase64 = /data:[\w\/\w]+;base64,/i.test(source);		
+		if (isBase64) manipulate(prepare(source, strength), callback, strength, attempts);
+		else get(source, callback, strength, attempts);
+
+	}	
+	return glitch;
+	
+	// Private methods
+	// A) Preparation
+	// 1) Load a raw image file using xhr, base64-encode it and start over
+	function get(source, callback, strength, attempts){
+					
+		var request = new XMLHttpRequest();
+		request.open('GET', source);
+		request.overrideMimeType('text/plain;charset=x-user-defined');
+		request.onload = function(){
+						
+			var response = request.response;
+			var match = response.substring(0,24).match(/[JFIF|GIF|PNG]+/i);
+			if (match){
+				var type = /JFIF/i.test(match[0]) ?	"jpeg" : match[0].toLowerCase();
+				var file = encode(response, "image/" + type);
+				glitch(file, callback, strength, attempts);
+			}
+			
+		};
+		request.onerror = function(){ error("The image could not be loaded") };
+		request.send();
+	
+	}
+	
+	// 1.1) base64-encode a binary representation of an image file
+	function encode(blob, type) {
+					
+		var data = [ "data:" + type + ";base64," ];
+		var i = 0, length = blob.length;
+		var	A, B, C;
+	
+		while (i < length){
+			A = blob.charCodeAt(i++) & 0xff;
+			if (i === length){
+				data.push(charset.charAt(A >> 2));
+				data.push(charset.charAt((A & 0x3) << 4));
+				data.push("==");
+				break;
+			}
+			B = blob.charCodeAt(i++);
+			if (i === length){
+				data.push(charset.charAt(A >> 2));
+				data.push(charset.charAt(((A & 0x3)<< 4) | ((B & 0xF0) >> 4)));
+				data.push(charset.charAt((B & 0xF) << 2));
+				data.push("=");
+				break;
+			}
+			C = blob.charCodeAt(i++);
+				data.push(charset.charAt(A >> 2));
+				data.push(charset.charAt(((A & 0x3) << 4) | ((B & 0xF0) >> 4)));
+				data.push(charset.charAt(((B & 0xF) << 2) | ((C & 0xC0) >> 6)));
+				data.push(charset.charAt(C & 0x3F));
+		}
+		
+		return data.join("");
+		
+	}
+	
+	// 2) Return an Object from a base64-encoded string that will be used later on
+	function prepare(base64, strength){
+	
+		var split = base64.split(",");
+		var data = distribute(split[1], strength);
+		
+		return {
+			header: split[0] + ",",
+			data: data,
+			slice: data[data.length - 1].length
+		};
+		
+		// Return an Array with an even distribution of the image data
+		function distribute(data){
+			var bytes = data.length;
+			var slice = Math.sqrt(bytes * (parseFloat("1e" + bytes.toString().length) / strength));
+			var count = bytes / slice;											
+			return data.match(new RegExp('.{1,' + Math.ceil(bytes / count) + '}', 'g'));
+		}
+		
+	}
+	
+	// B) Manipulation
+	// 1)
+	function manipulate(object, callback, strength, attempts){
+										
+		var data = object.data;
+		var result = [ object.header ];
+				
+		if (attempts--){
+			for (var i = 0, length = data.length; i < length; ++i){
+				result.push(alter(data[i], object.slice, attempts));	
+			}
+			validate(result.join(""), object, callback, strength, attempts);
+		}
+		else callback(object.header + data.join(""), false);
+		
+		function alter(string, length, attempts){
+			
+			var amount = limit(0|attempts / 10, 1, 10);
+			while (amount--){
+				var index = Math.random() * length|0;
+				var replacement = charset.charAt(Math.random() * 64|0);
+				string = string.substring(0, index) + replacement + string.substring(++index);
+			}
+			
+			return string;
+					
+		}
+			
+	}
+		
+	// 2) Check the image integrety, manipulate again when its broken, otherwise run the callback
+	function validate(result, base64, callback, attempts, strength){
+			
+		var canvas = document.createElement('canvas');
+		var context = canvas.getContext('2d');
+		var	image = new Image();
+		
+		image.onload = function(){
+		
+			context.drawImage(image, 0, 0, 2, 2);
+			var sample = context.getImageData(0, 0, 2, 2).data;
+			var	average = Math.floor((sample[12] + sample[13] + sample[14]) / 3);
+		
+			if (average <= 8 || average === 128) manipulate(base64, callback, strength, --attempts);
+			else callback(result, true);
+			
+		};
+		image.onerror = function(){ manipulate(base64, callback, strength, attempts) };
+		image.src = result;
+			
+	}
+	
+	// C) Utilities
+	function error(message){ console.error(message) }
+	
+	function limit(number, min, max){
+		return number < min ? min : number > max ? max : number;
+	}
+	
+}();
---
+++ readme.md
@@ -0,0 +1,32 @@
+Glitch JS is a small and dependency free script that manipulates images by making a random edits causing visual glitches to appear. Supported file types are jpeg, png, and (animated) gif.
+
+# Demo
+*coming soon…*
+
+# Usage
+## Examples
+```
+// Using a URL…
+glitch("/image.jpg", callback);
+
+// …or base64-encoded string.
+glitch(base64, callback);
+```
+
+The callback arguments are a base64-encoded image string and a boolean indicating whether the manipulation was successful.
+```
+function callback(image, succes){
+	// do whatever…   
+}
+```
+When glitch fails to create a valid manipulated image, the original image will be passed instead.
+
+## Parameters 
+- source {string}: a valid image URL or base64-encoded string is required for image manipulations to be made. Supported file types are jpeg, png, and gif.
+- callback {function}: a Function that will run upon completion.
+- strength {number}: a number between 5 and 100 to set the strength of the effect, defaults to 25.
+- attempts {number}: an integer between 5 and 100 that limits the manipulation attempts, defaults to 50.
+
+# Support
+Should work on IE9 and above aside from cross domain requests which is currently supported in IE10 and above.
+Tested using Chrome 50, Firefox 46 and Safari 9.1 on Mac OS X.
